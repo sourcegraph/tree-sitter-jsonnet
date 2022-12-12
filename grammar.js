a3ce9759bdd8820a206c6a0ae7476929941d16a7
pad <pad@r2c.dev>
2022-12-12 16:22:42 +0100

    support 'tailstrict' call annotation
    This is used in the standard jsonnet library (std.jsonnet)
    
    test plan:
    make test

--- grammar.js
+++ grammar.js
@@ -58,7 +58,7 @@ module.exports = grammar({
                     ),
                     seq($.super, ".", $.id),
                     seq($.super, "[", $.expr, "]"),
-                    seq($.expr, "(", optional($.args), ")"),
+                    seq($.expr, "(", optional($.args), ")", optional("tailstrict")),
                     $.id,
                     $.local_bind,
                     seq(
